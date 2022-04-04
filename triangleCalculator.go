6b120ec11d66b6b6baf3cdc02a4a1473eebee49a
HunKiat <hunkiat@gmail.com>
2022-04-04 18:30:18 +0800

    okay

--- triangleCalculator.go
+++ triangleCalculator.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func calculateSide(length1, length2, angle float64) float64 {
+func toRadians(angle float64) float64 {
+	return angle * (math.Pi / 180)
+}
 
+func calculateSide(length1, length2, angle float64) float64 {
+	// using the law of cosines to find the missing side
+	// use this...: a2 = b2 + c2 − 2bc cosA
+	// where a is the missing side, and A is the angle
+
+	// note that math.Cos uses radians
+	// radians := toRadians(angle)  // * _test.go requires the angle to be in radians
+
+	length3 := math.Sqrt((length1 * length1) + (length2 * length2) - (2 * length1 * length2 * math.Cos(angle)))
 	//Insert the code here
 
 	//Do not remove this line
@@ -22,7 +34,7 @@ func main() {
 	fmt.Scanln(&length1)
 	fmt.Println("Enter the second length of the triangle: ")
 	fmt.Scanln(&length2)
-	fmt.Println("Enter the angle between the two lengths: ")
+	fmt.Println("Enter the angle in degrees between the two lengths: ")
 	fmt.Scanln(&angle)
 
 	calculateSide(length1, length2, angle)
